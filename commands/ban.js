e61025990d05ddc0d0bb231b35a69312322ac6e8
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-05-25 22:35:20 +0100

    Tidy Up

--- commands/ban.js
+++ commands/ban.js
@@ -6,10 +6,14 @@ const settings = require('../settings.json');
 exports.run = async (client, message, args) => {
   const user = message.mentions.users.first();
   parseUser(message, user);
+  const modlog = client.channels.find("name",settings.moderationchannel);
   const caseNum = await caseNumber(client, modlog);
   if (message.mentions.users.size < 1) return message.reply('You must mention someone to ban them.').catch(console.error);
 
   const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
+  
+  message.guild.member(user).ban(reason);
+
   const embed = new RichEmbed()
   .setColor(0x00AE86)
   .setTimestamp()
--- commands/kick.js
+++ commands/kick.js
@@ -1,16 +1,19 @@
 const {RichEmbed} = require('discord.js');
 const {caseNumber} = require('../util/caseNumber.js');
 const {parseUser} = require('../util/parseUser.js');
+const {postToDefault} = require('../util/postToDefault.js');
 const settings = require('../settings.json');
 exports.run = async (client, message, args) => {
   const user = message.mentions.users.first();
   parseUser(message, user);
+  const modlog = client.channels.find("name",settings.moderationchannel);
   const caseNum = await caseNumber(client, modlog);
   if (message.mentions.users.size < 1) return message.reply('You must mention someone to kick them.').catch(console.error);
 
-  message.guild.member(user).kick();
-
   const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
+  
+  message.guild.member(user).kick(reason);
+  
   const embed = new RichEmbed()
   .setColor(0x00AE86)
   .setTimestamp()
