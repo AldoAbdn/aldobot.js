d64599721b1bc176bbd368c36f18ab7ce26d42c8
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-07-24 09:13:27 +0100

    Removing console logs

--- AldoBot.js
+++ AldoBot.js
@@ -25,7 +25,6 @@ async function setup(){
 
   //DB Setup
   let dbhelper = DBHelper.DBHelper;
-  console.log(dbhelper.url + " " + dbhelper.dbname);
   await dbhelper.connect();
   client.DBHelper = dbhelper;
 
