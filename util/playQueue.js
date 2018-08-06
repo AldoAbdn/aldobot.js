da735e8a1976f4fcfefe230dded2ee3f152a9b5f
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-08-06 22:52:05 +0100

    attempting fix

--- util/playQueue.js
+++ util/playQueue.js
@@ -10,6 +10,7 @@ exports.playQueue = (client,message) => {
           .then(connection => {
             console.log("connected");
             if (guild.dispatcher) {
+              
               if (guild.dispatcher.paused){
                 guild.dispatcher.resume();
               }
