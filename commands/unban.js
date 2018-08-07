803236b52eb9aba9210178ed08f712ecfc6bcd48
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-08-07 20:49:04 +0100

    fixed loop error

--- commands/unban.js
+++ commands/unban.js
@@ -4,18 +4,15 @@ exports.run = async(client, message, args) => {
   var userids = args[0].split(",");
   var users = [];
   const guild = message.guild;
-  console.log(userids);
   for (var userid of userids){
-    let user = await client.fetchUser(userid);
-    users.push(user);
+    users.push(await client.fetchUser(userid););
   }
   //Sets reason and unbanne
   client.unbanReason = reason;
   client.unbanAuth = message.author;
   //If no reason, returns
   if (reason.length < 1) return message.reply('You must supply a reason for the unban.');
-  for (var user in users){
-    console.log(user.tag);
+  for (var user of users){
     //Must pass a user resolvable
     if (!user) return message.reply('You must supply a User Resolvable, such as a user id.').catch(console.error);
     //Unbans user
