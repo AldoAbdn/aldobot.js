f41a3950ab2fac1527ae7d8af493bc0234cf34f2
Alistair Quinn <alistairaldo@hotmail.co.uk>
2020-04-06 18:58:12 +0100

    Fix to case number

--- util/caseNumber.js
+++ util/caseNumber.js
@@ -1,5 +1,5 @@
 async function caseNumber(client, modlog) {
-  const messages = await modlog.fetchMessages({limit:5});
+  const messages = await modlog.messages.cache.array();
   const log = messages.filter(m => m.author.id === client.user.id &&
     m.embeds[0] &&
     m.embeds[0].type === 'rich' &&
