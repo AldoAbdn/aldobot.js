5ec1907fddc7c590f46c5c96ab2357a8448a70c6
AldoAbdn <alistairaldo@hotmail.co.uk>
2020-04-20 00:26:17 +0100

    Fix to play queue

--- util/music.js
+++ util/music.js
@@ -21,7 +21,7 @@ exports.playQueue = (client,message) => {
             playSong(client, message, guild, voiceConnection).catch(error=>console.log('Error'+error));
           // Add Song To Queue
           } else if(guild.currentlyPlaying){
-            addToQueue(guild, message);
+            addToQueue(client, guild, message);
           } else {
             guild.playing = false;
           }
@@ -59,7 +59,7 @@ async function playSong(client, message, guild, voiceConnection){
   }
 }
 
-function addToQueue(guild, message){
+function addToQueue(client, guild, message){
   let url = "https://www.youtube.com/watch?v=" + guild.currentlyPlaying.related_videos[0].id;
   for (let vid of guild.currentlyPlaying.related_videos){
     if (guild.lastPlayed && vid.title != undefined && vid.title != guild.lastPlayed.title){
