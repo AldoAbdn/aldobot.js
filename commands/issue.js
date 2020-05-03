1363861c8a2440aeabd6080760e6740f53b68a84
AldoAbdn <alistairaldo@hotmail.co.uk>
2020-05-03 03:28:59 +0100

    Adding validation

--- commands/issue.js
+++ commands/issue.js
@@ -4,12 +4,16 @@ exports.run = async (client, message, args, perms, settings) => {
   const channels = message.guild.channels.cache;
   const log = channels.find(channel => channel.name === settings.supportchannel) || guild.channels.cache.find(channel => channel.name === settings.defaultchannel);
   const caseNumbers = args[0].split(",");
-  const newReason = args.slice(1).join(" ");
+  if(caseNumbers == null)
+    return message.reply('You must specify at least one case number');
+  const newReason = args.slice(1).join(" ") || '';
   const query = "**Issue:**";
   var supportticket;
   var logs;
   for (var caseNumber of caseNumbers){
     supportticket = channels.find(channel => channel.name === "support-ticket-"+caseNumber);
+    if(supportticket == null)
+      return message.reply('Invalid case number: ' + caseNumber + ' the number could be too large or small');
     logs = [log,supportticket];
     updateEmbedField(logs,client.user.id,caseNumber,query,newReason);
   }
--- commands/status.js
+++ commands/status.js
@@ -4,12 +4,17 @@ exports.run = async (client, message, args, perms, settings) => {
   const channels = message.guild.channels.cache;
   const log = channels.find(channel=>channel.name ===  settings.supportchannel) || channels.cache.find(channel => channel.name === settings.defaultchannel);
   const caseNumbers = args[0].split(",");
+  if(caseNumbers == null)
+    return message.reply('You must specify at least one case number');
+  const newReason = args.slice(1).join(" ") || '';
   const newReason = args.slice(1).join(" ");
   const query = "**Status:**";
   var supportticket;
   var logs;
   for (var caseNumber of caseNumbers){
     supportticket = channels.find(channel => channel.name === "support-ticket-"+caseNumber);
+    if(supportticket == null)
+      return message.reply('Invalid case number: ' + caseNumber + ' the number could be too large or small');
     logs = [log,supportticket];
     updateEmbedField(logs,client.user.id,caseNumber,query,newReason);
   }
