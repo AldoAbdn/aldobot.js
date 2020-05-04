1273aaf817dca335576c1933ade928ede3124b9e
AldoAbdn <alistairaldo@hotmail.co.uk>
2020-05-04 21:23:41 +0100

    trying to play related videos

--- util/music.js
+++ util/music.js
@@ -61,17 +61,7 @@ async function playSong(client, message, guild, voiceConnection){
 }
 
 function playRelatedVideos(client, guild, message, settings){
-  let url = "https://www.youtube.com/watch?v=" + guild.lastPlayed.related_videos[0].id;
-  for (let vid of guild.currentlyPlaying.related_videos){
-    if (guild.lastPlayed && vid.title != undefined && vid.title != guild.lastPlayed.title){
-      if (vid.id){
-        url = "https://www.youtube.com/watch?v=" + vid.id;
-      } else {
-        url = "https://www.youtube.com/watch?v=" + vid.video_id;
-      }
-      break;
-    }
-  }
+  let url = "https://www.youtube.com/watch?v=" + guild.lastPlayed.related_videos[0].id || guild.lastPlayed.related_videos[0].video_id;
   yt.getInfo(url).then(info=>{
       guild.queue.push(info);
       if(message.guild.currentPlaying){
