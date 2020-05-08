9c7fc259eeeadb56dbc4ef4b78acfd18c3940d70
AldoAbdn <alistairaldo@hotmail.co.uk>
2020-05-09 00:18:26 +0100

    Forgot to put .cache on member.roles in togglerole

--- commands/togglerole.js
+++ commands/togglerole.js
@@ -2,10 +2,9 @@ exports.run = async(client, message, args) => {
   //Set variables
   const members = message.mentions.members.array();
   const role = message.mentions.roles.first();
-  const guild = message.guild;
   for(var member of members){
     //Toggle role
-    if (member.roles.find("id",role.id)){
+    if (member.roles.cache.find("id",role.id)){
       //Removes roll
       member.removeRole(role).catch(console.error);
     } else {
